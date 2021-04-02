facaec9812eaa65f93879ea5a1bb0070ccb24caa
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-04-01 21:26:55 -0300

    Refactoring TodoDetailChangeAndDelete using generic class-based view

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,8 +1,6 @@
 from rest_framework import status, generics
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
 
 from .models import Todo
 from .serializers import TodoSerializer
@@ -13,28 +11,6 @@ class TodoListAndCreate(generics.ListCreateAPIView):
 	serializer_class = TodoSerializer
 
 
-class TodoDetailChangeAndDelete(APIView):
-	def get_object(self, pk):
-		try:
-			return Todo.objects.get(pk=pk)
-		except Todo.DoesNotExist:
-			raise NotFound()
-
-	def get(self, request, pk):
-		todo = self.get_object(pk)
-		serialized = TodoSerializer(todo)
-		return Response(serialized.data)
-
-	def put(self, request, pk):
-		todo = self.get_object(pk)
-		serialized = TodoSerializer(todo, data=request.data)
-
-		if serialized.is_valid():
-			serialized.save()
-			return Response(serialized.data)
-		return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-	def delete(self, request, pk):
-		todo = self.get_object(pk)
-		todo.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Todo.objects.all()
+	serializer_class = TodoSerializer
