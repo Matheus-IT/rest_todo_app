15bfe5263896d2bb166d5c86550cd0fb59c5545a
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-04-01 22:16:10 -0300

    Refactoring tests to make them work with the new model name

--- api_todo/app/tests/test_views.py
+++ api_todo/app/tests/test_views.py
@@ -2,21 +2,21 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
-from ..models import Todo
-from ..serializers import TodoSerializer
+from ..models import Task
+from ..serializers import TaskSerializer
 
 
-class TodoListAndCreateTests(APITestCase):
+class TaskListAndCreateTests(APITestCase):
 	def setUp(self):
 		self.AMOUNT_CREATED = 5
 
 		for i in range(self.AMOUNT_CREATED):
-			Todo.objects.create(
+			Task.objects.create(
 				name=f'{i}Task for tests'
 			)
 	
 	def test_get_tasks_list(self):
-		response = self.client.get(reverse('tasks-list'))
+		response = self.client.get(reverse('task-list'))
 
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(len(response.data), self.AMOUNT_CREATED)
@@ -25,7 +25,7 @@ class TodoListAndCreateTests(APITestCase):
 		data = {
 			'name': 'Simple task to test post request'
 		}
-		response = self.client.post(reverse('tasks-list'), data=data)
+		response = self.client.post(reverse('task-list'), data=data)
 
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 		self.assertEqual(response.data['name'], data['name'])
@@ -33,34 +33,34 @@ class TodoListAndCreateTests(APITestCase):
 	def test_post_new_invalid_task(self):
 		""" Sending invalid data """
 		data = {}
-		response = self.client.post(reverse('tasks-list'), data=data)
+		response = self.client.post(reverse('task-list'), data=data)
 
 		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class TodoDetailChangeAndDeleteTests(APITestCase):
+class TaskDetailChangeAndDeleteTests(APITestCase):
 	def setUp(self):
 		self.AMOUNT_CREATED = 2
 
 		for i in range(self.AMOUNT_CREATED):
-			Todo.objects.create(
+			Task.objects.create(
 				name=f'{i}Task for tests'
 			)
 	
 	def test_get_single_task(self):
-		response = self.client.get(reverse('tasks-detail', kwargs={'pk': 1}))
+		response = self.client.get(reverse('task-detail', kwargs={'pk': 1}))
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 	
 	def test_get_invalid_task(self):
 		""" Requesting a task that doesn't exist """
-		response = self.client.get(reverse('tasks-detail', kwargs={'pk': 999}))
+		response = self.client.get(reverse('task-detail', kwargs={'pk': 999}))
 		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 	def test_put_updating_task_name(self):
 		data = {
 			'name': 'This is a new name for testing purposes'
 		}
-		response = self.client.put(reverse('tasks-detail', kwargs={'pk': 1}), data=data)
+		response = self.client.put(reverse('task-detail', kwargs={'pk': 1}), data=data)
 		
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(response.data['name'], data['name'])
@@ -70,7 +70,7 @@ class TodoDetailChangeAndDeleteTests(APITestCase):
 			'name': 'This is a new name for testing purposes',
 			'done': True
 		}
-		response = self.client.put(reverse('tasks-detail', kwargs={'pk': 1}), data=data)
+		response = self.client.put(reverse('task-detail', kwargs={'pk': 1}), data=data)
 		
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(response.data['done'], data['done'])
@@ -80,19 +80,19 @@ class TodoDetailChangeAndDeleteTests(APITestCase):
 		data = {
 			'name': 'This is a new name for testing purposes'
 		}
-		response = self.client.put(reverse('tasks-detail', kwargs={'pk': 999}), data=data)
+		response = self.client.put(reverse('task-detail', kwargs={'pk': 999}), data=data)
 		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 	
 	def test_put_updating_invalid_data(self):
 		""" Test updating bad request """
 		data = {}
-		response = self.client.put(reverse('tasks-detail', kwargs={'pk': 1}), data=data)
+		response = self.client.put(reverse('task-detail', kwargs={'pk': 1}), data=data)
 		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 	
 	def test_delete_task(self):
-		response = self.client.delete(reverse('tasks-detail', kwargs={'pk': 1}))
+		response = self.client.delete(reverse('task-detail', kwargs={'pk': 1}))
 		self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 	
 	def test_delete_nonexisting_task(self):
-		response = self.client.delete(reverse('tasks-detail', kwargs={'pk': 999}))
+		response = self.client.delete(reverse('task-detail', kwargs={'pk': 999}))
 		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
